0ca685f98277d72f6234362452f9435d8bfadf2f
pythonfree <79115839983@ya.ru>
2022-01-24 23:41:33 +0300

    lesson 6

--- views/catalog.php
+++ views/catalog.php
@@ -4,11 +4,16 @@
     <h3><a href="/product/card/?id=<?= $item['id'] ?>"><?= $item['name'] ?></a></h3>
     <p><?= $item['description'] ?></p>
     <p><?= $item['price'] ?> руб.</p>
+    <button data-id="<?= $item['id'] ?>" class="buy" type="submit">Купить js</button>
     <form action="/basket/add" method="post">
         <input type="text" name="id" value="<?= $item['id'] ?>" hidden>
-        <button type="submit">Купить</button>
+        <button type="submit">Купить php</button>
     </form>
     <hr>
 <?php endforeach; ?>
 
 <a href="/product/catalog/?page=<?= $page ?>">Далее</a>
+
+<script>
+    let buttons = document.querySelectorAll('.buy');
+</script>
